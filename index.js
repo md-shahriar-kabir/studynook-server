d9f19151d3d1ec32559082d1ac85dec0aff9c6d8
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-05-19 16:50:04 +0600

    Update Patch with updated Data from Server

--- index.js
+++ index.js
@@ -59,12 +59,7 @@ async function run() {
       res.json(result)
     })
 
-    app.delete("/room/:id", async(req, res) => {
-      const {id} = req.params
-      const result =await roomCollection.deleteOne({_id:new ObjectId(id)})
-      res.json(result)
-    })
-
+   
 
 
     await client.db("admin").command({ ping: 1 });
