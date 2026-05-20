64a2596e1f3056c0bad3b926c25f5e0a9cc69f2e
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-05-21 00:37:24 +0600

    fixing Token data in Delete Booking

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ async function run() {
 
     });
 
-    app.delete('/booking/:bookingId', async(req, res) => {
+    app.delete('/booking/:bookingId',verifyToken, async(req, res) => {
       const {bookingId} = req.params
       const result =await bookingCollection.deleteOne({_id:new ObjectId(bookingId)})
       res.json(result)
